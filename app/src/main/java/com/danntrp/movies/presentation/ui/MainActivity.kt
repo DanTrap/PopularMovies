181070ac7a1764f37477ce64d14654ba6a0de8c2
Daniil Trapeznikov <danntrp@gmail.com>
2023-02-18 21:28:08 +0300

    Migration to Navigation using navigation components

--- app/src/main/java/com/danntrp/movies/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/danntrp/movies/presentation/ui/MainActivity.kt
@@ -1,70 +1,63 @@
 package com.danntrp.movies.presentation.ui
 
 import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
+import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.danntrp.movies.R
 import com.danntrp.movies.databinding.ActivityMainBinding
-import com.danntrp.movies.presentation.ui.description.MovieDescriptionFragment
-import com.danntrp.movies.presentation.ui.favorite.FavoriteMovieFragment
-import com.danntrp.movies.presentation.ui.navigation.Navigation
-import com.danntrp.movies.presentation.ui.popular.PopularMovieFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), Navigation {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val popularFragment = PopularMovieFragment()
-    private val favoriteFragment = FavoriteMovieFragment()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                val popFragment = supportFragmentManager.findFragmentByTag("pop-fragment")
-                val favFragment = supportFragmentManager.findFragmentByTag("fav-fragment")
-                if (popFragment != null && popFragment.isVisible) {
-                    finish()
-                } else if (favFragment != null && favFragment.isVisible) {
-                    showPopularFragment()
-                } else {
-                    pop()
+        setSupportActionBar(binding.toolBar)
+
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+
+        navController = navHost.findNavController()
+
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.popularMovieFragment, R.id.favoriteMovieFragment))
+
+        binding.bottomNavView.setupWithNavController(navController)
+
+        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.movieDescriptionFragment -> {
+                    binding.bottomNavView.visibility = View.GONE
+                }
+                else -> {
+                    binding.bottomNavView.visibility = View.VISIBLE
                 }
             }
-        })
-
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragmentContainer, popularFragment, "pop-fragment")
-                .commit()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragmentContainer, favoriteFragment, "fav-fragment")
-                .addToBackStack(null)
-                .hide(favoriteFragment)
-                .commit()
         }
     }
 
-    override fun showFavoriteFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .hide(popularFragment)
-            .show(favoriteFragment)
-            .commit()
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
-    override fun showPopularFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .hide(favoriteFragment)
-            .show(popularFragment)
-            .commit()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun showDescriptionFragment(id: Int) {
--- app/src/main/java/com/danntrp/movies/presentation/ui/popular/PopularMovieFragment.kt
+++ app/src/main/java/com/danntrp/movies/presentation/ui/popular/PopularMovieFragment.kt
@@ -1,19 +1,30 @@
 package com.danntrp.movies.presentation.ui.popular
 
+import android.app.Activity
+import android.app.SearchManager
+import android.app.SearchableInfo
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.core.os.bundleOf
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.danntrp.movies.R
 import com.danntrp.movies.core.util.Resource
 import com.danntrp.movies.databinding.FragmentPopularMovieBinding
 import com.danntrp.movies.presentation.adapters.MarginItemDecorator
 import com.danntrp.movies.presentation.adapters.MovieAdapter
-import com.danntrp.movies.presentation.ui.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -21,26 +32,37 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
-class PopularMovieFragment : Fragment(R.layout.fragment_popular_movie) {
+class PopularMovieFragment : Fragment(R.layout.fragment_popular_movie), MenuProvider {
 
     private lateinit var binding: FragmentPopularMovieBinding
     private lateinit var movieAdapter: MovieAdapter
-    private lateinit var navigation: Navigation
+    private lateinit var searchableInfo: SearchableInfo
+    private lateinit var menuHost: MenuHost
     private val movieViewModel: MovieViewModel by viewModels()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        navigation = context as Navigation
+        val searchManager = context.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchableInfo = searchManager.getSearchableInfo((context as Activity).componentName)
+        menuHost = context as MenuHost
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPopularMovieBinding.bind(view)
 
-        setupRecyclerView()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.STARTED)
+
+        movieAdapter = MovieAdapter()
+
+        binding.popularMoviesRecyclerView.apply {
+            adapter = movieAdapter
+            layoutManager = LinearLayoutManager(activity)
+            addItemDecoration(MarginItemDecorator(resources.getDimensionPixelSize(R.dimen.recycler_view_items_margin)))
+        }
 
         movieAdapter.setOnItemClickListener { id ->
-            navigation.showDescriptionFragment(id)
+            findNavController().navigate(R.id.action_popularMovieFragment_to_movieDescriptionFragment, bundleOf("movie-id" to id))
         }
 
         movieAdapter.setOnItemLongClickListener {
@@ -54,7 +76,6 @@ class PopularMovieFragment : Fragment(R.layout.fragment_popular_movie) {
                     binding.progressBar.visibility = View.GONE
                     binding.networkLayout.visibility = View.GONE
                     binding.progressBarRepeat.visibility = View.GONE
-                    Log.d("ABOBA", "POP FRAGMENT SUCCESS")
                     response.data?.let {
                         movieAdapter.differ.submitList(response.data)
                     }
@@ -79,26 +100,23 @@ class PopularMovieFragment : Fragment(R.layout.fragment_popular_movie) {
             }
         }
 
-        binding.popularButton.setOnClickListener {
-            movieViewModel.getPopularMovies()
-        }
-
-        binding.favoriteButton.setOnClickListener {
-            navigation.showFavoriteFragment()
-        }
-
         binding.repeatButton.setOnClickListener {
             binding.progressBarRepeat.visibility = View.VISIBLE
             movieViewModel.getPopularMovies()
         }
     }
 
-    private fun setupRecyclerView() {
-        movieAdapter = MovieAdapter()
-        binding.popularMoviesRecyclerView.apply {
-            adapter = movieAdapter
-            layoutManager = LinearLayoutManager(activity)
-            addItemDecoration(MarginItemDecorator(resources.getDimensionPixelSize(R.dimen.recycler_view_items_margin)))
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        (menu.findItem(R.id.searchButton).actionView as SearchView).apply {
+            setSearchableInfo(searchableInfo)
+
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(query: String?) = true
+
+                override fun onQueryTextSubmit(query: String?) = true
+            })
         }
     }
+
+    override fun onMenuItemSelected(menuItem: MenuItem) = true
 }
